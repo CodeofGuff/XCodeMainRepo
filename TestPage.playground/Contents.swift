d90f5a811e37e3f97d1f6b1822f73792f59d0734
CodeofGuff <guffcode@gmail.com>
2024-09-18 20:06:12 -0700

    Ok

--- TestPage.playground/Contents.swift
+++ TestPage.playground/Contents.swift
@@ -1,9 +1,28 @@
 import Cocoa
 
-var greeting = "Hello, playground"
-for i in 1...5 {
-	print(greeting, i)
+
+func boolToWord(_ bool: Bool) -> String {
+	
+	if bool == true {
+		return "Yes"
+	} else {
+		return "No"
+	}
+	
 }
 
+var untrue = boolToWord(false)
+
+print(untrue)
 
 
+func greet(_ name: String) -> String {
+	
+	return "Hello \(name) how are you doing today?"
+	
+	
+}
+
+var diane = greet("Diane")
+
+print(diane)
